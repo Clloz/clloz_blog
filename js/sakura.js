275524f0390522db6639671e10699c33e6cbe532
clloz <22374308@qq.com>
2017-05-03 17:06:14 +0800

    sakura set

--- js/sakura.js
+++ js/sakura.js
@@ -54,9 +54,9 @@
         var rect = el.getBoundingClientRect();
 
         return (
-            rect.top >= 0 &&
+            rect.top >= 90 &&
                 rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.bottom <= (window.innerHeight - 100 || document.documentElement.clientHeight - 100) &&
                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
             );
     }
